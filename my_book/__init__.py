a9af46f61ce8d7299a510b6f8dc7cbd00175ebe7
PetroffSky <ds9657@yandex.ru>
2023-05-24 09:29:42 +0300

    Добавлена функция для ответов да/нет. Добавлена управляющая функция.

--- my_book/__init__.py
+++ my_book/__init__.py
@@ -1,4 +1,23 @@
-def select_answer():
+def answer_yesno():
+    answer = None
+    flag = False
+    while not flag:
+        input_answer = input("Введите да - если ответ утвердителен.\n"
+                             "Введите нет - если ответ отрицателен.\n"
+                             "Ожидаю ввод: ")
+        if input_answer in ["lf", "yes", "да", "нуы"]:
+            answer = input_answer
+            flag = True
+        elif input_answer == "exit":
+            break
+        else:
+            print("Введён недопустимый ответ! Повторите ввод!\n")
+            flag = False
+
+    return answer
+
+
+def answer_number():
     mylist = []
     flag = False
     while not flag:
@@ -12,10 +31,20 @@ def select_answer():
         else:
             mylist.append(int(input_answer))
             flag = False
+
     return mylist
+
+
+def main():
+    print("Вводим числа?")
+    main_answer = answer_yesno()
+    if main_answer in ["lf", "yes", "да", "нуы"]:
+        print(answer_number())
+    else:
+        pass
 
 
 
 if __name__ == "__main__":
     print("Начинаем работать!")
-    print(select_answer())
+    main()
